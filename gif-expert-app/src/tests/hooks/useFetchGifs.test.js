7680488fe1c58fa816272e1bc37ec1f009e6e4ca
Milumon <Milumon@outlook.com>
2021-07-29 12:01:28 -0500

    test final

---
+++ gif-expert-app/src/tests/hooks/useFetchGifs.test.js
@@ -0,0 +1,27 @@
+import { useFetchGifs } from "../../hooks/useFetchGifs";
+import {renderHook} from '@testing-library/react-hooks'
+describe('Pruebas en useFetchGifs', () => {
+    test('Debe de retornar el estado inicial', async () => {
+
+        const { result, waitForNextUpdate } = renderHook(() => useFetchGifs('GFRIEND'));
+        const {data, loading } = result.current;
+        await waitForNextUpdate();
+
+
+        expect( data ).toEqual([]);
+        expect( loading ).toBe(true)
+        
+    })
+
+    test('Debe de retornar un arreglo de imgs y el loading en false', async () => {
+        const { result, waitForNextUpdate } = renderHook(() => useFetchGifs('GFRIEND'))
+
+        await waitForNextUpdate();
+
+        const { data, loading } = result.current;
+        expect( data.length ).toBe(10);
+        expect( loading ).toBe(false)
+    })
+    
+    
+});
