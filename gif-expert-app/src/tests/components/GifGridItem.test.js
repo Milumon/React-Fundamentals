40a3086bf27416beae8a7ee59e25036709728529
Milumon <Milumon@outlook.com>
2021-07-29 09:32:55 -0500

    test part 2

--- gif-expert-app/src/tests/components/GifGridItem.test.js
+++ gif-expert-app/src/tests/components/GifGridItem.test.js
@@ -4,11 +4,32 @@ import { GifGridItem } from '../../components/GifGridItem';
 
 
 describe('Pruebas en <GifGridItem />', () => {
-    
+        
     const title= 'Mi titulito :D';
     const url= 'https://localhost/imagen.jpg';
+    const wrapper = shallow( <GifGridItem title={title} url={url} /> )
+
     test('Debe de mostrar el componente correctamente', () => {
-        const wrapper = shallow( <GifGridItem title={title} url={url} /> )
+       
         expect (wrapper).toMatchSnapshot();
+
+    });
+
+    test('Debe tener el title igual al prop', () => {
+        const p = wrapper.find('p') 
+        expect(p.text().trim()).toBe(title)
+    });
+
+    test('Debe de tener la imagen igual al props url y alt', () => {
+        const img = wrapper.find('img')
+        expect(img.prop('src')).toBe(url)
+        expect(img.prop('alt')).toBe(title)
+    });
+
+    test('Debe tener fadeIn como className', () => {
+        const div = wrapper.find('div')
+        //expect(div.prop('className').split(' ')[2]).toBe('animate__fadeIn')
+        expect(div.prop('className').includes('animate__fadeIn')).toBe(true)
+
     });
 });
